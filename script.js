d341b60d0d414ab66ca25d045ce883b5a9e8d742
Carolina Rodrigues <carolina3.ads@gmail.com>
2025-08-01 08:39:05 -0300

    alteração de nomes dos arquivos

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 // função para adicionar tarefa 
  
  function adicionarTarefa() {
-      // let mensagem = "Tarefa adicionada com sucesso!";
-            document.getElementById("mensagem").textContent = mensagem;
 
-            let inputTarefa = document.getElementById("inputTarefa")
-            let tarefa = inputTarefa.value
-            document.getElementById("mensagem").textContent = tarefa;
-            
-            let listaTarefas = document.getElementById("listaTarefas")
+             //recebe valor do input do usuário
+            const inputTarefa = document.getElementById("inputTarefa")
+            let tarefa = inputTarefa.value.trim()
+
+            const mensagem = document.getElementById("mensagem")
+
+            // se o valor de input for vazio, adicionar mensagem de erro
+
+            if (tarefa == "") {
+            let mensagemErro = "Digite uma tarefa para ser adicionada à lista!"
+            mensagem.textContent = mensagemErro
+            }
+
+            //mensagem de tarefa adicionada com sucesso
+            else {
+            let mensagemSucesso = "Tarefa adicionada com sucesso!"
+            mensagem.textContent = mensagemSucesso
+            }
+       
+             //cria novo item (li) e insere na (lista ul)
+            const listaTarefas = document.getElementById("listaTarefas")
             let novaTarefa = document.createElement("li")
 
             novaTarefa.textContent = tarefa
             listaTarefas.appendChild(novaTarefa)
 
+            //limpa o input do usuário
             inputTarefa.value = ""
+
+            
+
+            
+
 
     }
